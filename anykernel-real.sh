810acd2cd694b34385fe2c6c95f59b3542b6581a
dotkit <ewprjkt@proton.me>
2023-07-14 16:17:18 +0000

    AK3: rebase from SkyWalker
    Change-Id: Icad1ab08da668d1a4847b2cd61a7bc1c40bf8f19
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -7,7 +7,7 @@ properties() { '
 kernel.string=ExampleKernel by osm0sis @ xda-developers
 kernel.for=KernelForDriver
 kernel.compiler=SDPG
-kernel.made=Ryuuji @ItsRyuujiX
+kernel.made=dotkit @fakedotkit
 kernel.version=44xxx
 kernel.type=xxx
 message.word=blablabla
